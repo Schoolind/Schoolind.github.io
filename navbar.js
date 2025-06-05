3827ed2e92dd5fc85b8dc6b3cb5d93410d18a04e
bigbopperbops <Kbrummeyer1@gmail.com>
2025-06-05 15:17:13 -0400

    25.01

--- navbar.js
+++ navbar.js
@@ -9,7 +9,7 @@ function createNavBar(pathPrefix = '') {
                     <path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z"></path>
                 </svg>
             </button>
-            <button class="nav-button" onclick="window.location.href='../../movie-of-the-day.html'">
+            <button class="nav-button" onclick="window.location.href='../../movie-of-the-week.html'">
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="nav-icon">
                     <rect x="2" y="7" width="20" height="15" rx="2" ry="2"></rect>
                     <polyline points="17 2 12 7 7 2"></polyline>
