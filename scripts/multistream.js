2d06969867461ca823e3ae40032f5271e5d065d8
bigbopperbops <Kbrummeyer1@gmail.com>
2025-07-18 12:48:10 -0400

    47.06

--- scripts/multistream.js
+++ scripts/multistream.js
@@ -104,7 +104,7 @@
             { name: "NBC philadelphia", url: "https://subrugopuciblchlvl6uqa666p23rig.happy-ending.site/b46c64f505d77d9c3ba37c2f3037f0f3.m3u8", type: "m3u8" },
             { name: "NBC california👑", url: "https://topembed.pw/channel/NBCSCalifornia[USA]", type: "iframe" },
             { name: "NBC bayarea", url: "https://subrugopuciblchlvl6uqa666p23rig.happy-ending.site/5411235d278f9abfce9db28c140374d5.m3u8", type: "m3u8" },
-            { name: "NBC boston👑", url: "https://topembed.pw/channel/NBCSBoston[USA]", type: "iframe" },
+            { name: "NBC boston", url: "https://subrugopuciblchlvl6uqa666p23rig.happy-ending.site/DF01290A3852048FA5C6688724BCE1FA.m3u8", type: "m3u8" },
             { name: "NBC golf", url: "https://popcdn.day/cue.php?stream=GOLFChannel", type: "iframe" },
         ]
     },
